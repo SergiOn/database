c4258f43ae9fc47a81ecd794fac018cb25e6474f
SergiOn <onishhenko@gmail.com>
2020-03-23 23:45:51 +0200

    #sqlzoo # 1 SELECT name

---
+++ sqlzoo/1-SELECT-name.sql
@@ -0,0 +1,107 @@
+/*
+world
+name          continent
+Afghanistan   Asia
+Albania       Europe
+Algeria       Africa
+Andorra       Europe
+Angola        Africa
+....
+*/
+
+/*
+1
+*/
+SELECT name FROM world
+  WHERE name LIKE 'y%'
+
+/*
+2
+*/
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+/*
+3
+*/
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+/*
+4
+*/
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+/*
+5
+*/
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+
+/*
+6
+*/
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+/*
+7
+*/
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+/*
+8
+*/
+SELECT name FROM world
+  WHERE name LIKE '_t%'
+  ORDER BY name
+
+/*
+9
+*/
+SELECT name FROM world
+  WHERE name LIKE '%o__o%'
+
+/*
+10
+*/
+SELECT name FROM world
+  WHERE name LIKE '____'
+
+/*
+11
+*/
+SELECT name
+  FROM world
+  WHERE name = capital
+
+/*
+12
+*/
+SELECT name
+  FROM world
+  WHERE capital = concat(name, ' City')
+
+/*
+13
+*/
+SELECT capital, name
+  FROM world
+  WHERE capital LIKE concat('%', name, '%')
+
+/*
+14
+*/
+SELECT capital, name
+  FROM world
+  WHERE capital LIKE concat(name, '_%')
+
+/*
+15
+*/
+SELECT name, REPLACE(capital, name, '') as ex
+  FROM world
+  WHERE capital LIKE concat(name, '_%')
+
